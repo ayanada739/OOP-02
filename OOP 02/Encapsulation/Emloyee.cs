1e8eb1c8afe800c62e67d125a71c901c0de32b40
ayanada739 <ayanada739@gmail.com>
2024-07-12 20:06:47 +0300

    Pre Encapsulation

---
+++ OOP 02/Encapsulation/Emloyee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_02.Encapsulation
+{
+    internal struct Employee
+    {
+        #region Atributes
+
+        //public int Id;
+        //public string Name;
+        //public decimal Salary;
+        #endregion
+ 
+        #region Constructors
+        //public Employee(int id, string name, decimal salary)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Salary = salary;
+        //}
+        #endregion
+
+        #region Methods
+        //public override string ToString()
+        //{
+        //    return $"Id = {Id}\nName = {Name}\nSalary = {Salary:c}";
+        //} 
+        #endregion
+
+
+
+    }
+}
--- OOP 02/Program.cs
+++ OOP 02/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace OOP_02
+﻿using OOP_02.Encapsulation;
+using System.Collections.Generic;
+
+namespace OOP_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Employee employee = new Employee(name: "Aya", id: 10, salary: 3000); ;
+
+            ////Id = 10
+            ////Name = "Aya"
+            ////Salary = $3,000.00
+
+            //Console.WriteLine(employee);
+
+            //employee.Id = 20; //Set Id Directly Through Attribute
+            //Console.WriteLine(employee.Id); //Get Id Directly Through Attribute
+
         }
     }
 }
